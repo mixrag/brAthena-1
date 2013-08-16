104a19a0510c1eb84c3be82742753f727ad7510c
Carlos Heitor Lain <carlos.lain@virtuati.com.br>
2013-08-16 03:03:24 -0300

    r2395

---
+++ src/config/const.h
@@ -0,0 +1,118 @@
+/****************************************************************************!
+*                _           _   _   _                                       *    
+*               | |__  _ __ / \ | |_| |__   ___ _ __   __ _                  *  
+*               | '_ \| '__/ _ \| __| '_ \ / _ \ '_ \ / _` |                 *   
+*               | |_) | | / ___ \ |_| | | |  __/ | | | (_| |                 *    
+*               |_.__/|_|/_/   \_\__|_| |_|\___|_| |_|\__,_|                 *    
+*                                                                            *
+*                                                                            *
+* \file src/config/const.h                                                   *
+* Descrição Primária.                                                        *
+* Descrição mais elaborada sobre o arquivo.                                  *
+* \author brAthena, rAthena                                                  *
+* \date ?                                                                    *
+* \todo ?                                                                    *  
+*****************************************************************************/
+
+#ifndef _H_CONSTANTS_
+#define _H_CONSTANTS_
+
+/**
+ * Checagens de segurança para tempo de intervalo nos diálogos com npcs.
+ **/
+#if SECURE_NPCTIMEOUT_INTERVAL <= 0
+	#error SECURE_NPCTIMEOUT_INTERVAL should be at least 1 (1s)
+#endif
+#if NPC_SECURE_TIMEOUT_INPUT < 0
+	#error NPC_SECURE_TIMEOUT_INPUT cannot be lower than 0
+#endif
+#if NPC_SECURE_TIMEOUT_MENU < 0
+	#error NPC_SECURE_TIMEOUT_MENU cannot be lower than 0
+#endif
+#if NPC_SECURE_TIMEOUT_NEXT < 0
+	#error NPC_SECURE_TIMEOUT_NEXT cannot be lower than 0
+#endif
+
+/**
+ * Sufixo para arquivos no banco de dados da renovação/pré-renovação
+ **/
+#if VERSION == 1
+	#define DBPATH "_re.txt"
+#elif VERSION == 0
+	#define DBPATH "_pre-re.txt"
+#else
+	#define DBPATH "_ot.txt"
+#endif
+
+/**
+ * DefType
+ *
+ *
+#if VERSION == 1
+ */
+	typedef short defType;
+	#define DEFTYPE_MIN SHRT_MIN
+	#define DEFTYPE_MAX SHRT_MAX
+/*
+#else
+	typedef signed char defType;
+	#define DEFTYPE_MIN CHAR_MIN
+	#define DEFTYPE_MAX CHAR_MAX
+#endif
+*/
+
+/* Definições para corrigir vários gcc warnings */
+#ifdef __64BIT__
+	#define __64BPRTSIZE(y) (intptr)y
+#else
+	#define __64BPRTSIZE(y) y
+#endif
+
+/* Cálculos de ATCMD_FUNC(mobinfo) HIT e FLEE */
+#if VERSION == 1
+	#define MOB_FLEE(mob) ( mob->lv + mob->status.agi + 100 )
+	#define MOB_HIT(mob)  ( mob->lv + mob->status.dex + 150 )
+	#define RE_SKILL_REDUCTION(){ \
+			wd.damage = battle_calc_elefix(src, target, skill_id, skill_lv, battle_calc_cardfix(BF_WEAPON, src, target, nk, s_ele, s_ele_, wd.damage, 0, wd.flag), nk, n_ele, s_ele, s_ele_, false, flag.arrow); \
+			if(flag.lh) \
+				wd.damage2 = battle_calc_elefix(src, target, skill_id, skill_lv, battle_calc_cardfix(BF_WEAPON, src, target, nk, s_ele, s_ele_, wd.damage2, 1, wd.flag), nk, n_ele, s_ele, s_ele_, true, flag.arrow); \
+		}
+#else
+	#define MOB_FLEE(mob) ( mob->lv + mob->status.agi )
+	#define MOB_HIT(mob)  ( mob->lv + mob->status.dex )
+#endif
+
+/* Modo de renovação para modificações de dano conforme o nível */
+#ifdef RENEWAL_LVDMG
+	#define RE_LVL_DMOD(val) \
+		if( status_get_lv(src) > 100 && val > 0 ) \
+			skillratio = skillratio * status_get_lv(src) / val;
+	#define RE_LVL_MDMOD(val) \
+		if( status_get_lv(src) > 100 && val > 0) \
+			md.damage = md.damage * status_get_lv(src) / val;
+	/* Armadilhas Especiais Sentinelas */
+	#define RE_LVL_TMDMOD() \
+		if( status_get_lv(src) > 100 ) \
+			md.damage = md.damage * 150 / 100 + md.damage * status_get_lv(src) / 100;
+#else
+	#define RE_LVL_DMOD(val) 
+	#define RE_LVL_MDMOD(val)
+	#define RE_LVL_TMDMOD()
+#endif
+
+/* Número máximo de carrinhos para clientes de 2012 */
+#if PACKETVER >= 20120201
+	#define NEW_CARTS
+	#define MAX_CARTS 9
+#else
+	#define MAX_CARTS 5
+#endif
+
+// Redução do tempo de conjuração variável da renovação.
+#ifdef RENEWAL_CAST
+	#define VARCAST_REDUCTION(val){ \
+		if( (varcast_r += val) != 0 && varcast_r >= 0 ) \
+			time = time * (1 - (float)min(val, 100) / 100); \
+	}
+#endif
+#endif /* _H_CONSTANTS_ */
---
+++ src/config/secure.h
@@ -0,0 +1,55 @@
+/****************************************************************************!
+*                _           _   _   _                                       *    
+*               | |__  _ __ / \ | |_| |__   ___ _ __   __ _                  *  
+*               | '_ \| '__/ _ \| __| '_ \ / _ \ '_ \ / _` |                 *   
+*               | |_) | | / ___ \ |_| | | |  __/ | | | (_| |                 *    
+*               |_.__/|_|/_/   \_\__|_| |_|\___|_| |_|\__,_|                 *    
+*                                                                            *
+*                                                                            *
+* \file src/config/secure.h                                                  *
+* Descrição Primária.                                                        *
+* Descrição mais elaborada sobre o arquivo.                                  *
+* \author brAthena, rAthena                                                  *
+* \date ?                                                                    *
+* \todo ?                                                                    *  
+*****************************************************************************/
+
+#ifndef _CONFIG_SECURE_H_
+#define _CONFIG_SECURE_H_
+
+/**
+ * Tempo de habilitação no diálogo para npcs.
+ * Quando habilitado, todos os diálogos com npcs são desabilitados conforme número de segundos.
+ * - ? : Tempo em segundos (ex: 10)
+ * - 0 : Disabilitado
+ **/
+
+//#define SECURE_NPCTIMEOUT
+
+/**
+ * number of seconds to 'timeout' if the user is on idle for longer than the value allowed after a 'input' field is displayed.
+ * default: 180
+ **/
+#define NPC_SECURE_TIMEOUT_INPUT 180
+
+/**
+ * number of seconds to 'timeout' if the user is on idle for longer than the value allowed after a 'menu' is displayed.
+ * default: 60
+ **/
+#define NPC_SECURE_TIMEOUT_MENU 60
+
+/**
+ * number of seconds to 'timeout' if the user is on idle for longer than the value allowed after a 'next' button is displayed.
+ * default: 60
+ **/
+#define NPC_SECURE_TIMEOUT_NEXT 60
+
+/**
+ * (Secure) Optional NPC Dialog Timer
+ * @requirement : SECURE_NPCTIMEOUT must be enabled
+ * Minimum Interval Between timeout checks in seconds
+ * Default: 1s
+ **/
+#define SECURE_NPCTIMEOUT_INTERVAL 1
+
+#endif // _CONFIG_SECURE_H_
